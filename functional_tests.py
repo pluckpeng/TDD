70e2d22014fc6b65db8edfe611bf32741f21dc35
WangJiaxin <2644371397@qq.com>
2022-05-10 09:21:26 +0800

    before reconstruct

--- functional_tests.py
+++ functional_tests.py
@@ -11,8 +11,8 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-    # def tearDown(self):
-    #     self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
@@ -20,18 +20,20 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        # self.assertEqual(
+        #     inputbox.get_attribute('placeholder'),
+        #     'Enter a to-do item'
+        # )
+        # inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_element(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),"New to-do item did not appear in table"
-        )
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows])
         self.fail('Finish the test!')
 
 
