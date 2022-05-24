c22473274e6af8b2fd217d5e9ad7b25f769c85cd
WangJiaxin <2644371397@qq.com>
2022-05-24 08:15:46 +0800

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,12 @@
 import time
 
 from selenium import webdriver
-import unittest
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from django.test import LiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,25 +20,18 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text,[row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
-        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title), "Browser title was: " + self.browser.title
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder')
                          , 'Enter a to-do item')
-        # She types "Buy peacock feather" into a text box(Edith's hobby
-        # is tying fly-fishing lures)
+
         inputbox.send_keys('Buy peacock feathers')
 
-        # when she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -50,12 +43,5 @@ class NewVisitorTest(unittest.TestCase):
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        # self.assertTrue(
-        #     any(row.text == '1: Buy peacock feathers' for row in rows),
-        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        # )
         self.fail('Finish the test!')
 
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
