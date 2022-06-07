91838e1ccd07f89ae953cc38869167ae0e5bc4d4
Zirui Peng <pluckpeng@bupt.edu.cn>
2022-06-07 22:48:40 +0800

    FINAL VER

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,15 +3,25 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
+import os
+
+STAGING_SERVER = '139.196.19.41'
 
 MAX_WAIT=10
 
-class NewVisitorTest(LiveServerTestCase):
+# class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):    
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
+        # staging_server=os.environ.get('STAGING_SERVER')
+        staging_server=True
+        if staging_server:
+            self.live_server_url='http://'+STAGING_SERVER
+        self.browser.set_window_size(1024,768)
 
     def tearDown(self):
         self.browser.quit()
@@ -62,7 +72,7 @@ class NewVisitorTest(LiveServerTestCase):
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url,'/lists/.+')
         self.browser.quit()
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
 
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
@@ -81,4 +91,16 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers',page_text)
         self.assertIn('Buy milk',page_text)
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        # self.browser.set_window_size(1024,768)
 
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        # inputbox.send_keys('testing')
+        # inputbox.send_keys(Keys.ENTER)
+        # self.wait_for_row_in_list_table('1: testing')
+        self.assertAlmostEqual(
+            inputbox.location['x']+inputbox.size['width']/2,
+            512,
+            delta=10
+        )
